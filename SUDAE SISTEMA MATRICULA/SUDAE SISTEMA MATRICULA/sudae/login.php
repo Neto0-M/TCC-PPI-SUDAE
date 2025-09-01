14ac062a579141448375f00fe7ed698d7b5a80fa
Joao0816A <joao.2023304486@aluno.iffar.edu.br>
2025-08-31 22:17:15 -0300

    login.php

--- SUDAE SISTEMA MATRICULA/SUDAE SISTEMA MATRICULA/sudae/login.php
+++ SUDAE SISTEMA MATRICULA/SUDAE SISTEMA MATRICULA/sudae/login.php
@@ -36,7 +36,7 @@ if (isset($_POST['login'])) {
   <title>Login - SUDAE</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <!-- Bootstrap CSS -->
+
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
   <style>
@@ -132,8 +132,9 @@ if (isset($_POST['login'])) {
   </div>
 </div>
 
-<!-- Bootstrap JS -->
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+
 
