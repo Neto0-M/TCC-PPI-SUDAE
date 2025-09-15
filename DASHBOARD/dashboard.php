0a5a8cb71ad13561ba220753d270d6538df1c8fb
Antony <antonytheaita@gmail.com>
2025-09-15 16:06:17 -0300

    login

--- DASHBOARD/dashboard.php
+++ DASHBOARD/dashboard.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once __DIR__ . './atas/conexao.php';
+require_once __DIR__ . '././ATAS/conexao.php';
 
 // Verifica se o usuário está logado
 if (!isset($_SESSION['usuario'])) {
